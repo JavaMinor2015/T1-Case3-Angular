0583a1b6d9bfac8bff08bed459f47d34742e6df0
Remco Westerhoud <remcowesterhoud@msn.com>
2016-01-06 19:52:11 +0100

    Fill shopping cart

--- src/main/webapp/app/scripts/controllers/cart.js
+++ src/main/webapp/app/scripts/controllers/cart.js
@@ -6,10 +6,27 @@
  * # CartController
  * Controller
  */
-angular.module('kantileverAngular').controller('cartController', function ($scope) {
-    $scope.products = [];
+angular.module('kantileverAngular').controller('cartController', function ($scope, ShoppingCartService) {
+    $scope.products = ShoppingCartService.products;
     $scope.addToCart = function (product) {
-        $scope.products.push(product);
+        var isInCart = false;
+        for (var i = 0; i < $scope.products.length; i++) {
+            if ($scope.products[i] == product) {
+                isInCart = true;
+                $scope.products[i].amount += 1;
+                break;
+            }
+        }
+        if (!isInCart) {
+            $scope.products.push(product);
+        }
+    };
+    $scope.getCartItemAmount = function () {
+        var amount = 0;
+        for (var i = 0; i < $scope.products.length; i++) {
+            amount += $scope.products[i].amount;
+        }
+        return amount;
     };
 });
 //# sourceMappingURL=cart.js.map
--- src/main/webapp/app/scripts/controllers/cart.ts
+++ src/main/webapp/app/scripts/controllers/cart.ts
@@ -6,11 +6,34 @@
  * # CartController
  * Controller
  */
-angular.module('kantileverAngular').controller('cartController', function($scope) {
+angular.module('kantileverAngular').controller('cartController', function($scope, ShoppingCartService) {
 
-  $scope.products = [];
+  $scope.products = ShoppingCartService.products;
 
   $scope.addToCart = function (product) {
-    $scope.products.push(product);
+    var isInCart = false;
+
+    for (var i = 0; i < $scope.products.length; i++){
+      if ($scope.products[i] == product){
+        isInCart = true;
+        $scope.products[i].amount += 1;
+        break;
+      }
+    }
+
+    if (!isInCart){
+      $scope.products.push(product);
+    }
   };
+
+  $scope.getCartItemAmount = function(){
+    var amount = 0;
+
+    for (var i = 0; i < $scope.products.length; i++){
+      amount += $scope.products[i].amount;
+    }
+
+    return amount;
+  }
+
 });
