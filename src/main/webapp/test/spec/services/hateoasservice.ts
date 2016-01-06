9a980a8745523d2aa2e9b8a41a54b8f09465ebd2
Tom Jaspers <info@javaminor2015.com>
2016-01-06 21:22:02 +0100

    HateoasService + shoppingcartservice fix for the build

--- src/main/webapp/test/spec/services/hateoasservice.ts
+++ src/main/webapp/test/spec/services/hateoasservice.ts
@@ -1,18 +1,18 @@
 /// <reference path="../../../typings/angularjs/angular-mocks.d.ts" />
 /// <reference path="../../../typings/jasmine/jasmine.d.ts" />
-/// <reference path="../../../app/scripts/services/shoppingcartservice.ts" />
+/// <reference path="../../../app/scripts/services/hateoasservice.ts" />
 
 'use strict';
 
-describe('Service: shoppingCartService', () => {
+describe('Service: HateoasService', () => {
 
   // load the service's module
   beforeEach(module('kantileverAngular'));
 
   // instantiate service
-  var shoppingCartService;
-  beforeEach(inject(_shoppingCartService_ => {
-    shoppingCartService = _shoppingCartService_;
+  var HateoasService;
+  beforeEach(inject(_HateoasService_ => {
+    HateoasService = _HateoasService_;
   }));
 
 });
