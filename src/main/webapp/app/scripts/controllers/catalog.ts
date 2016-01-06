d06bf607e7e6666c6b2e3c3a142eae988b518731
Remco Westerhoud <remcowesterhoud@msn.com>
2016-01-06 14:02:44 +0100

    Simple productoverview

---
+++ src/main/webapp/app/scripts/controllers/catalog.ts
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name KantileverAngular.controller:CatalogCtrl
+ * @description
+ * # CatalogCtrl
+ *
+ */
+
+angular.module('kantileverAngular')
+  .controller('CatalogCtrl', function ($scope, $location) {
+
+    $scope.products = [
+      {
+        id: "98765",
+        name: "bike",
+        price: 125.5,
+      },
+      {
+        id: "56789",
+        name: "Wheel",
+        price: 45.2,
+      }
+    ];
+
+    $scope.addToCart = function(product){
+      //TODO: Add the product to the cart
+    };
+
+    $scope.redirect = function(product){
+      $location.url('/main.html');
+    }
+
+  });
