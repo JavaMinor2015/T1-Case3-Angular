3305f8b77fa53ca45e0ba1eddea1cfdcaf4d767a
Remco Westerhoud <remcowesterhoud@msn.com>
2016-01-06 14:48:22 +0100

    Added table options

--- src/main/webapp/app/scripts/controllers/catalog.js
+++ src/main/webapp/app/scripts/controllers/catalog.js
@@ -8,20 +8,26 @@
  */
 angular.module('kantileverAngular')
     .controller('CatalogCtrl', function ($scope, $location) {
+    $scope.sortType = 'name'; // set the default sort type
+    $scope.sortReverse = false; // set the default sort order
+    $scope.searchProduct = ''; // set the default search/filter term
     $scope.products = [
         {
             id: "98765",
-            name: "bike",
-            price: 125.5
+            name: "Wheel",
+            price: 45.5
         },
         {
             id: "56789",
-            name: "Wheel",
-            price: 45.2
+            name: "Bike",
+            price: 125.2
         }
     ];
     $scope.addToCart = function (product) {
         //TODO: Add the product to the cart
+    };
+    $scope.redirect = function (product) {
+        $location.url('/main.html');
     };
 });
 //# sourceMappingURL=catalog.js.map
--- src/main/webapp/app/scripts/controllers/catalog.ts
+++ src/main/webapp/app/scripts/controllers/catalog.ts
@@ -11,16 +11,20 @@
 angular.module('kantileverAngular')
   .controller('CatalogCtrl', function ($scope, $location) {
 
+    $scope.sortType     = 'name'; // set the default sort type
+    $scope.sortReverse  = false;  // set the default sort order
+    $scope.searchProduct  = '';   // set the default search/filter term
+
     $scope.products = [
       {
         id: "98765",
-        name: "bike",
-        price: 125.5,
+        name: "Wheel",
+        price: 45.5,
       },
       {
         id: "56789",
-        name: "Wheel",
-        price: 45.2,
+        name: "Bike",
+        price: 125.2,
       }
     ];
 
