48983e0fd5fe55889ad0b79d385611a4e8fd136b
Remco Westerhoud <remcowesterhoud@msn.com>
2016-01-11 15:40:10 +0100

    Customer Service created

---
+++ src/main/webapp/app/scripts/services/customerService.js
@@ -0,0 +1,35 @@
+'use strict';
+angular.module('kantileverAngular').service('customerService', function ($resource) {
+    var customerResource = $resource('htpp://localhost:6789/customers/:customerId', { customerId: '@customerId' }, { update: { method: 'PUT' } });
+    this.getCustomers = function () {
+        return customerResource.query();
+    };
+    this.getCustomer = function (id) {
+        return customerResource.get({ customerId: id });
+    };
+    this.postCustomer = function (hateoasItem) {
+        var customer = getContent(hateoasItem);
+        customerResource.save(customer, function () { }, function () {
+            handleError();
+        });
+    };
+    this.updateCustomer = function (hateoasItem) {
+        var customer = getContent(hateoasItem);
+        customerResource.update({ customerId: customer.id }, customer, function () { }, function () {
+            handleError();
+        });
+    };
+    this.deleteCustomer = function (hateoasItem) {
+        var customer = getContent(hateoasItem);
+        customerResource.delete({ customerId: customer.id }, function () { }, function () {
+            handleError();
+        });
+    };
+    var getContent = function (hateoasItem) {
+        return hateoasItem.content;
+    };
+    var handleError = function () {
+        console.log('error');
+    };
+});
+//# sourceMappingURL=customerService.js.map
