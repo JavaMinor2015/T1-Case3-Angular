66e47ca5e2a1742f55ccda7a126f8cc84da0251d
Tom Jaspers <info@javaminor2015.com>
2016-01-12 12:43:02 +0100

    imagemin gruntfile delete

--- src/main/webapp/Gruntfile.js
+++ src/main/webapp/Gruntfile.js
@@ -296,17 +296,6 @@ module.exports = function (grunt) {
        dist: {}
      },
 
-    imagemin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.app %>/images',
-          src: '{,*/}*.{png,jpg,jpeg,gif}',
-          dest: '<%= yeoman.dist %>/images'
-        }]
-      }
-    },
-
     svgmin: {
       dist: {
         files: [{
@@ -412,7 +401,6 @@ module.exports = function (grunt) {
       ],
       dist: [
         'copy:styles',
-        'imagemin',
         'svgmin'
       ]
     },
