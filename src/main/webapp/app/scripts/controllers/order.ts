4e3c4343fcc5d18fea87f3ee29ee8d51a8be68fa
Laurens Oomen <oomen.laurens@gmail.com>
2016-01-19 15:01:22 +0100

    merge fixes

--- src/main/webapp/app/scripts/controllers/order.ts
+++ src/main/webapp/app/scripts/controllers/order.ts
@@ -12,9 +12,9 @@ angular.module('kantileverAngular').controller('orderController', function ($sco
   $scope.order = orderService.newOrder;
   $scope.orderInfo = [];
 
-  $scope.completeOrder = function() {
+  $scope.completeOrder = function () {
     console.info('Thank you for your order!');
-    if ($scope.order.orderId === undefined){
+    if ($scope.order.orderId === undefined) {
       orderService.postOrder($scope.order);
     }
     else {
@@ -23,20 +23,21 @@ angular.module('kantileverAngular').controller('orderController', function ($sco
     $scope.order = orderService.emptyCart();
   };
 
-  $scope.getOrder = function() {
+  $scope.getOrder = function () {
     var orderid = $routeParams.orderId;
     $scope.orderInfo = orderService.setOrderInfo(orderid);
   };
 
-  $scope.getOrderList = function() {
+  $scope.getOrderList = function () {
     return orderService.getAllOrder();
 
-  $scope.cancelOrder = function(order){
+  }
+  $scope.cancelOrder = function (order) {
     order.orderStatus = 'CANCELLED';
     orderService.updateOrder(order);
   };
 
-  $scope.editOrder = function(order){
+  $scope.editOrder = function (order) {
     localStorage.setItem('order', JSON.stringify(order));
     orderService.newOrder = order;
     $scope.order = orderService.newOrder;
